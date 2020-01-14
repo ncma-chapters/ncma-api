350a2f8de3aa29432c5f53b64adc25ca5c5bcf40
Kevin Mircovich <kmircovich1@gmail.com>
2020-01-13 21:50:05 -0500

    remove comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,4 @@ class ApplicationController < ActionController::API
   include JSONAPI::ActsAsResourceController
 
   abstract
-
-  # rescue_from ActiveRecord::RecordNotFound, with: :jsonapi_render_not_found
-  # rescue_from NotAuthorizedError, with: :reject_forbidden_request
 end
