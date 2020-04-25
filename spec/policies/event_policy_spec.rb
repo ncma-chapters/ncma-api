ab405475fad12177dfebb60fe0465ed2b86aec39
Kevin Mircovich <kmircovich1@gmail.com>
2020-04-25 19:52:01 -0400

    Update event_policy_spec.rb

--- spec/policies/event_policy_spec.rb
+++ spec/policies/event_policy_spec.rb
@@ -2,11 +2,38 @@ require 'rails_helper'
 
 RSpec.describe EventPolicy, type: :policy do
 
-  let(:subject) { described_class.new nil, build(:event) }
+  let(:event) { build(:event) }
 
   describe 'create?' do
-    it 'returns false' do
+    it 'returns false when user is nil' do
+      subject = described_class.new nil, event
       expect(subject.create?).to eq(false)
+    end
+
+    it 'returns false when user is not in EventManagers group' do
+      user = double
+      subject = described_class.new user, event
+
+      expect(user).to receive(:in_group).with(
+        'EventManagers'
+      ).and_return(
+        false
+      )
+
+      expect(subject.create?).to eq(false)
+    end
+
+    it 'returns true when user is in EventManagers group' do
+      user = double
+      subject = described_class.new user, event
+
+      expect(user).to receive(:in_group).with(
+        'EventManagers'
+      ).and_return(
+        true
+      )
+
+      expect(subject.create?).to eq(true)
     end
   end
 end
