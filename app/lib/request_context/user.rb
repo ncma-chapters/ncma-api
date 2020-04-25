a12d9050945f0b40bbed0d648051b3be7f22b709
Kevin Mircovich <kmircovich1@gmail.com>
2020-04-25 19:51:15 -0400

    refactor request_context/user.rb

--- app/lib/request_context/user.rb
+++ app/lib/request_context/user.rb
@@ -6,16 +6,24 @@ class RequestContext
       @original_payload = id_token_payload
     end
 
+    def id
+      original_payload['sub']
+    end
+
     def expires_at
-      Time.at(original_payload[:exp]).to_datetime
+      Time.at(original_payload['exp']).to_datetime
     end
 
     def issued_at
-      Time.at(original_payload[:iat]).to_datetime
+      Time.at(original_payload['iat']).to_datetime
     end
 
-    def sub
-      original_payload[:sub]
+    def groups
+      original_payload['cognito:groups'] || []
+    end
+
+    def in_group(group_name)
+      groups.include?(group_name)
     end
   end
 end
