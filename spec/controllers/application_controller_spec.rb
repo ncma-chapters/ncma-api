774d4607c124f7944d6245a817fa378f4320fab5
Kevin Mircovich <kmircovich1@gmail.com>
2020-04-26 02:00:17 -0400

    More specs for application controller: jwt parsing and errors

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -65,4 +65,62 @@ RSpec.describe ApplicationController, :type => :controller do
       end
     end
   end
+
+  describe '#context' do
+    describe 'when auth header is not present' do
+      it 'a RequestContext with a nil user' do
+        request = double(authorization: nil)
+
+        expect(subject).to receive(:request).and_return(request).at_least(1).times
+
+        expect(subject.context).to be_instance_of(RequestContext)
+        expect(subject.context.user).to eq(nil)
+      end
+    end
+
+    describe 'when auth header is poorly formatted' do
+      it 'raises JWT::DecodeError' do
+        request = double(authorization: 'bad.jwt.token')
+
+        expect(subject).to receive(:request).and_return(request).at_least(1).times
+
+        expect { subject.context }.to raise_error(JWT::DecodeError)
+      end
+    end
+
+    describe 'when auth header is invalid' do
+      context 'when expired' do
+        it 'raises JWT::ExpiredSignature ' do
+          token = JWT.encode({ exp: 1 }, 'secret')
+          request = double(authorization: "Bearer #{token}")
+
+          expect(subject).to receive(:request).and_return(request).at_least(2).times
+
+          expect { subject.context }.to raise_error(JWT::DecodeError, 'Signature has expired')
+        end
+      end
+    end
+
+    describe 'when auth header is valid' do
+      context 'when expired' do
+        it 'raises JWT::ExpiredSignature ' do
+          token_payload = {
+            'sub' => SecureRandom.uuid,
+            'iat' => DateTime.now.to_i,
+            'exp' => (DateTime.now + 1.hour).to_i,
+            'cognito:groups' => ['Admin']
+          }
+
+          token = JWT.encode(token_payload, 'secret')
+          request = double(authorization: "Bearer #{token}")
+
+          expect(subject).to receive(:request).and_return(request).at_least(2).times
+
+          expect(subject.context).to be_instance_of(RequestContext)
+          expect(subject.context.user).to be_instance_of(RequestContext::User)
+          expect(subject.context.user.original_payload).to eq(token_payload)
+        end
+      end
+    end
+  end
 end
